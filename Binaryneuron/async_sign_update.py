9a3e45220283569de2ab2c98b8efad37ca840276
kesheng xu <keshengxu@hotmail.com>
2017-12-23 22:11:18 -0300

    Create async_sign_update.py

---
+++ Binaryneuron/async_sign_update.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 23 17:36:56 2017
+keshengxuu@gmail.com
+@author: ksxuu
+"""
+
+import numpy as np
+
+state_s0 = np.ones(1000)
+nr_neurons = 4
+s = np.random.poisson(10, size=(nr_neurons,30))
+Tetime =np.zeros(s.shape)
+Tetime = np.cumsum(s,axis = 1)
+
+# nevent is the flag of  updating state 
+nevent = np.zeros(s.shape[0],dtype=int)
+a=0
+for i in range(np.amax(Tetime)): # looping for the simulation time
+    for j in range(s.shape[0]):   # looping for the numbe of the neurons
+        if nevent[j] <s.shape[1] and  i == Tetime[j,nevent[j]]:
+            nevent[j] = nevent[j] + 1
+            print i,j
